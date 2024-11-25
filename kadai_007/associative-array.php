02adc2fedaa338f7a8d9c68a9f7e43ef7a1f2a96
shuhei-fujimura <s.fujimura0406@gmail.com>
2024-11-25 12:57:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>associative-array</title>
+</head>
+<body>
+    <?php
+    $kadai = ['name' => 'onion','price' => '200','weight' => '160'];
+    print_r($kadai);
+    ?>
+</body>
+</html>
