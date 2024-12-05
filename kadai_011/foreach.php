8e9f27464a8e00e2c8db5db47b0cce229804c61d
shuhei-fujimura <s.fujimura0406@gmail.com>
2024-12-05 17:49:00 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>foreach</title>
+</head>
+<body>
+    <p>
+        <?php
+        $tamanegi = ['名前' => '玉ねぎ','値段' => '200','産地' => '北海道'];
+        foreach($tamanegi as $key => $value) {
+            echo "{$key} : {$value}<br>";
+        }
+        ?>
+    </p>
+</body>
+</html>
